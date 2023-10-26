14bba247a8ea1ece7bef04602db78e883da7f475
kajjtek <149022191+kajjtek@users.noreply.github.com>
2023-10-26 11:36:32 +0200

    Update 1.1.c

--- 1.1.c
+++ 1.1.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-printf("A B R A K A D A B R A \n")
-printf(" A B R A K A D A B R  \n")
-printf("  A B R A K A D A B \n")
+printf("A B R A K A D A B R A \n");
+printf(" A B R A K A D A B R  \n");
+printf("  A B R A K A D A B \n");
 printf("   A B R A K A D A \n");
-printf("    A B R A K A D \n")
-printf("     A B R A K A \n")
-printf("      A B R A K \n")
-printf("       A B R A \n")
-printf("        A B R \n")
-printf("         A B \n")
-printf("          A \n")
+printf("    A B R A K A D \n");
+printf("     A B R A K A \n");
+printf("      A B R A K \n");
+printf("       A B R A \n");
+printf("        A B R \n");
+printf("         A B \n");
+printf("          A \n");
 
 
 
